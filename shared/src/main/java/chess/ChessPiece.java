d4e2d42daf700c0b6f5bafa7a63489cd2c14c2f3
clare <cbwood04@icloud.com>
2026-09-09 21:33:59 -0600

    move calculator added, BishopMoveTests passed.

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -1,5 +1,6 @@
 package chess;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
@@ -11,7 +12,12 @@ import java.util.List;
  */
 public class ChessPiece {
 
+    private final ChessGame.TeamColor pieceColor;
+    private final PieceType type;
+
     public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
+        this.pieceColor = pieceColor;
+        this.type = type;
     }
 
     /**
@@ -30,14 +36,14 @@ public class ChessPiece {
      * @return Which team this chess piece belongs to
      */
     public ChessGame.TeamColor getTeamColor() {
-        throw new RuntimeException("Not implemented");
+        return pieceColor;
     }
 
     /**
      * @return which type of chess piece this piece is
      */
     public PieceType getPieceType() {
-        throw new RuntimeException("Not implemented");
+        return type;
     }
 
     /**
@@ -48,6 +54,77 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        return List.of();
+        MoveCalc movesList = new MoveCalc(board, myPosition, this);
+        return movesList.listMoves();
+    }
+    
+    private class MoveCalc {
+        ChessBoard board;
+        ChessPosition myPosition;
+        ChessPiece myPiece;
+        int curRow;
+        int curCol;
+        
+        public MoveCalc (ChessBoard board, ChessPosition myPosition, ChessPiece myPiece) {
+            this.board = board;
+            this.myPosition = myPosition;
+            this.myPiece = myPiece;
+            this.curRow = myPosition.getRow();
+            this.curCol = myPosition.getColumn();
+        }
+        
+        public Collection<ChessMove> listMoves() {
+            return switch (myPiece.getPieceType()) {
+                case PieceType.BISHOP -> bishopMoves();
+                case PieceType.KING -> bishopMoves();
+                case PieceType.KNIGHT -> bishopMoves();
+                case PieceType.PAWN -> bishopMoves();
+                case PieceType.QUEEN -> bishopMoves();
+                case PieceType.ROOK -> bishopMoves();
+            };
+        };
+        
+        public boolean checkSpot (int row, int col, Collection<ChessMove> lst) {
+            if (row < 1 || row > 8 || col < 1 || col > 8) {
+                return false;
+            }
+            
+            ChessPosition newPos = new ChessPosition(row,col);
+            
+            if (board.getPiece(newPos) == null) {
+                lst.add(new ChessMove(myPosition, newPos, null));
+                return true;
+            } else if (board.getPiece(newPos).getTeamColor() != myPiece.getTeamColor()) {
+                lst.add(new ChessMove(myPosition, newPos, null));
+                return false;
+            }
+
+            return false;
+        }
+
+        public Collection<ChessMove> bishopMoves() {
+            Collection<ChessMove> bishopList = new ArrayList<>();
+            for (int i = 1; i<=8; i++) {
+                if (!checkSpot(curRow + i, curCol + i, bishopList)) {
+                    break;
+                }
+            }
+            for (int i = 1; i<=8; i++) {
+                if (!checkSpot(curRow - i, curCol + i, bishopList)) {
+                    break;
+                }
+            }
+            for (int i = 1; i<=8; i++) {
+                if (!checkSpot(curRow + i, curCol - i, bishopList)) {
+                    break;
+                }
+            }
+            for (int i = 1; i<=8; i++) {
+                if (!checkSpot(curRow - i, curCol - i, bishopList)) {
+                    break;
+                }
+            }
+            return bishopList;
+        }
     }
 }
